28ea6f620457cf32f0f06d98f1d532b25a1702e8
gosha124214 <gosha.lih@gmail.com>
2025-05-06 00:58:36 +0300

    06.05.2025V3

---
+++ InCubeAPI/WebApplication1/Classes/Authorization/EmailRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.Classes.Authorization
+{
+    public class EmailRequest
+    {
+        public string Email { get; set; }
+        public bool IsLogin { get; set; }
+    }
+}
---
+++ InCubeAPI/WebApplication1/Classes/Authorization/VerifyCodeRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Classes.Authorization
+{
+    public class VerifyCodeRequest
+    {
+        public string Email { get; set; }
+        public string Code { get; set; }
+        public bool IsLogin { get; set; }
+    }
+}
